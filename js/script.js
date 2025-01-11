539a1614c3a3e40fd378b7e9a5938877fac53318
Anton Titov <italisman04@gmail.com>
2025-01-12 01:03:17 +0200

    Sixth commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { headerScript } from "../js/header-script.js";
+import { mainScript } from "../js/main-script.js";
 
 ////////////////////////////////////HEADER///////////////////////////////////////
 fetch('../html/header.html')
@@ -7,5 +8,13 @@ fetch('../html/header.html')
         document.getElementById('header').innerHTML = html;
         headerScript();
     })
-    .catch(error => console.error('Error loading the file:', error));
+    .catch(error => console.error('Error loading the header:', error));
 
+////////////////////////////////////MAIN///////////////////////////////////////
+fetch ('../html/main.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('main').innerHTML = html;
+        mainScript();
+    })
+    .catch(error => console.error('Error loading the main:', error))
